57e9b88a7013fa20722c402dbec85df61b8df53c
Lucas D'Avila <lucas@lucasdavi.la>
2015-06-18 01:00:16 -0300

    Validate mail config

--- config/initializers/0_env.rb
+++ config/initializers/0_env.rb
@@ -6,9 +6,17 @@ module Initializers
       YAML.load_file 'config/mail.yml'
     end
 
-    module_function :mail_config
+    def validate_mail_config!
+      if mail_config.blank? || mail_config[ENV['ENV']].blank?
+        raise "Please define the mail config for the #{ENV['ENV']} env in the config/mail.yml file."
+      end
+    end
+
+    module_function :mail_config, :validate_mail_config!
   end
 end
+
+Initializers::Env.validate_mail_config!
 
 Initializers::Env.mail_config[ENV['ENV']].each do |name, value|
   ENV['mail_' + name] = value.to_s
