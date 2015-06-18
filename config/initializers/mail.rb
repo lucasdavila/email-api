34b46a703c735811ca207ef9207b1b614ad11ade
Lucas D'Avila <lucas@lucasdavi.la>
2015-06-18 16:45:57 -0300

    Only send domain to Mail lib, if it was defined.

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,10 +1,16 @@
 Mail.defaults do
-  delivery_method ENV['mail_delivery_method'],
-                  address: ENV['mail_address'],
-                  port: ENV['mail_port'],
-                  enable_starttls_auto: ENV['mail_enable_starttls_auto'],
-                  user_name: ENV['mail_user_name'],
-                  password: ENV['mail_password'],
-                  authentication: ENV['mail_authentication'],
-                  domain: ENV['mail_domain']
+  options = {
+    address: ENV['mail_address'],
+    port: ENV['mail_port'],
+    enable_starttls_auto: ENV['mail_enable_starttls_auto'],
+    user_name: ENV['mail_user_name'],
+    password: ENV['mail_password'],
+    authentication: ENV['mail_authentication']
+  }
+
+  # prevent Net::SMTPSyntaxError - 501 5.0.0 HELO requires domain address error
+  # if there is no domain defined.
+  options[:domain] = ENV['mail_domain'] if ENV['mail_domain']
+
+  delivery_method ENV['mail_delivery_method'], options
 end
