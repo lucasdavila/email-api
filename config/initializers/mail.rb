e51faa104ca0e68087ba74cfdbc1a6937001846e
Lucas D'Avila <lucas@lucasdavi.la>
2015-06-18 01:22:29 -0300

    Fix config.

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,7 +1,7 @@
 Mail.defaults do
   delivery_method ENV['mail_delivery_method'],
                   address: ENV['mail_address'],
-                  port: ENV['mail_'],
+                  port: ENV['mail_port'],
                   enable_starttls_auto: ENV['mail_enable_starttls_auto'],
                   user_name: ENV['mail_user_name'],
                   password: ENV['mail_password'],
