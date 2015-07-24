b77dd3006cea157c2e7db1e639f125d655c64c0c
Lucas D'Avila <lucas@lucasdavi.la>
2015-07-24 12:28:49 -0300

    Set data by default as an empty array.

--- lib/models/prospect.rb
+++ lib/models/prospect.rb
@@ -1,3 +1,11 @@
 class Prospect < ActiveRecord::Base
   validates :email, :list_name, presence: true
+
+  before_save :set_default_data
+
+  protected
+
+  def set_default_data
+    self.data = {} if data.nil?
+  end
 end
--- spec/models/prospect_spec.rb
+++ spec/models/prospect_spec.rb
@@ -1,4 +1,25 @@
 RSpec.describe Prospect, :type => :model do
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_presence_of(:list_name) }
+
+  describe 'before save' do
+    it 'sets data with an empty array' do
+      expect(subject.data).to be_nil
+      subject.run_callbacks :save
+      expect(subject.data).to eq({})
+    end
+
+    context 'when data is already set' do
+      let(:data) { { 'foo' => 'bar' } }
+
+      before do
+        subject.data = data
+      end
+
+      it 'not changes' do
+        subject.run_callbacks :save
+        expect(subject.data).to eq data
+      end
+    end
+  end
 end
